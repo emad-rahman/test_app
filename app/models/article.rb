9fbe4a59789439a540ea630911009eb5aae657ac
Emad Rahman <emadrahman@d142-058-220-050.wireless.sfu.ca>
2016-11-25 15:35:50 -0800

    created new template action and route

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
-class Article < ApplicationRecord
+class Article < ActiveRecord::Base
+
+validates :title, :presence => true, :length => {:minimum => 2}
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161125210803) do
+ActiveRecord::Schema.define(version: 20161125221852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(version: 20161125210803) do
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
