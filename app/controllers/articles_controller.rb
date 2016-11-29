9e65ae783402da47403580abca8a952f22ae83ce
Emad Rahman <emadpoke@gmail.com>
2016-11-29 00:41:36 -0800

    added destory article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
 
+
   def index
     @articles = Article.order("id ASC")
   end
@@ -36,7 +37,12 @@ class ArticlesController < ApplicationController
     end
   end
 
-
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was successfully destroyed"
+    redirect_to articles_path
+  end
 
   private
   def article_params
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  include Rails.application.routes.url_helpers
+
+    def cancel_link
+      return link_to 'Cancel', request.env["HTTP_REFERER"],
+        :class => 'cancel',
+        :confirm => 'Are you sure? Any changes will be lost.'
+    end
+
+    
 end
