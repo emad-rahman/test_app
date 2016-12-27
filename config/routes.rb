2f0ab46f7a9ec3d10a0d9bc8b33fc03aa733f743
Emad Rahman <emadpoke@gmail.com>
2016-12-26 18:36:20 -0800

    starting devise development

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :user_for_blogs
   resources :articles
   root 'welcome#home'
   get '/welcome/home' => 'welcome#home'
---
+++ test/models/user_for_blog_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UserForBlogTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
